79b08a8cdb5abafc02771e7d3f5957481fd3003e
xiaofp622 <xiao.mx.fp@gmail.com>
2021-01-10 14:02:25 +0800

    s

--- Hello_world.java
+++ Hello_world.java
@@ -2,5 +2,6 @@
 public class Hello_world{
 
 public static void main(String[] args){
-			System.out.println("hello world");} 
+System.out.println(":sfsf");			
+System.out.println("hello world");} 
 			}
