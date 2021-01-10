9f3fc400c707312746ce97fb4a00964e209c16e3
xiaofp622 <xiao.mx.fp@gmail.com>
2021-01-10 16:10:36 +0800

    main

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@
 public class Hello_world{
 
 public static void main(String[] args){
-System.out.println("master");}
+public int xx ; 
 
-}System.out.println("mxdgggsg");
+System.out.println("master");}
+System.out.println("mxdgggsg");
+
+
+}
 
 			}
